4124b694dd467400e30e8a206a1fd3bad1667fa7
kaustubh87 <kaustubh87@gmail.com>
2022-09-30 10:55:44 -0400

    Added boilerplate code

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+  //Canvas setup
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 1500;
+  canvas.height = 500;
+
+  class InputHandler {}
+
+  class Projectile {}
+
+  class Particle {}
+
+  class Player {}
+
+  class Enemy {}
+
+  class Layer {}
+
+  class Background {}
+
+  class UI {}
+
+  class Game {}
+});
